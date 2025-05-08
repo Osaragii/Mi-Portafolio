b41c360106bb3c00e0a1d276fba613e2799dd576
Osaragii <dalaidev9@gmail.com>
2025-05-08 14:18:39 +0530

    Added 3 new components

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -6,6 +6,9 @@ import CanvasLoader from "../components/CanvasLoader";
 import { useMediaQuery } from "react-responsive";
 import { calculateSizes } from "../constants/index.js";
 import Target from "../components/Target.jsx";
+import ReactLogo from "../components/ReactLogo.jsx";
+import Cube from "../components/Cube.jsx";
+import Rings from "../components/Rings.jsx";
 
 const Hero = () => {
   const isSmall = useMediaQuery( { maxWidth: 480 } );  
@@ -18,7 +21,7 @@ const Hero = () => {
     <section className='min-h-screen w-full flex flex-col relative'>
         <div className='w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3'>
             <p className='sm:text-3xl text-2xl font-medium text-white text-center font-generalsans'>Hi, I am Dev <span className='waving-hand'>👋</span></p>
-            <p className='hero_tag text-gray_gradient'>Building Products and Brands</p>
+            <p className='hero_tag text-gray_gradient'>Building Products & Brands</p>
         </div>
 
         <div className='w-full h-full absolute inset-0'>
@@ -38,6 +41,9 @@ const Hero = () => {
                 />
                     <group>
                       <Target position={sizes.targetPosition} />
+                      <ReactLogo position={sizes.reactLogoPosition} />
+                      <Cube position={sizes.cubePosition}/>
+                      <Rings position={sizes.ringPosition}/>
                     </group>
                     <ambientLight intensity={1} />
                     <directionalLight position={[10, 10, 10]} intensity={0.5} />
